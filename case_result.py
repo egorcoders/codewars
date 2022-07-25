6c3c9961a8e2b97e879487bdfd6e693fa82de7cb
Egor <egorcoders@gmail.com>
2022-07-26 01:54:16 +0500

    Solved with 1st attempt. Best solution must be revised.

---
+++ case_result.py
@@ -0,0 +1,7 @@
+case_result_dct = {
+    "no one likes this": [],
+    "Peter likes this": ["Peter"],
+    "Jacob and Alex like this": ["Jacob", "Alex"],
+    "Max, John and Mark like this": ["Max", "John", "Mark"],
+    "Alex, Jacob and 2 others like this": ["Alex", "Jacob", "Mark", "Max"]
+}
