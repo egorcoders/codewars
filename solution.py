44802272a5d0124ab310d929faabf437b2813c16
Egor <egorcoders@gmail.com>
2022-07-26 01:59:55 +0500

    removed

--- solution.py
+++ solution.py
@@ -1,3 +1,4 @@
+# my solution
 def likes(names):
     ln = len(names)
     if ln == 0:
@@ -10,3 +11,15 @@ def likes(names):
         return f"{names[0]}, {names[1]} and {names[2]} like this"
     else:
         return f"{names[0]}, {names[1]} and {ln - 2} others like this"
+
+
+# best solution
+def likes2(names):
+    n = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this',
+        2: '{} and {} like this',
+        3: '{}, {} and {} like this',
+        4: '{}, {} and {others} others like this'
+    }[min(4, n)].format(*names[:3], others=n-2)
