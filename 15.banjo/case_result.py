e09b9a073a36c3b9d7686b9264dbc64ab8c53db6
Egor <egorcoders@gmail.com>
2022-08-03 08:40:21 +0500

    wrtie in a single line

---
+++ 15.banjo/case_result.py
@@ -0,0 +1,6 @@
+case_result_lst = [
+    ["martin", "martin does not play banjo", 'test 1'],
+    ["Rikke", "Rikke plays banjo", 'test 2'],
+    ["bravo", "bravo does not play banjo", 'test 3'],
+    ["rolf", "rolf plays banjo", 'test 4'],
+]
---
+++ 15.banjo/solution.py
@@ -0,0 +1,12 @@
+# my solution
+def are_you_playing_banjo(name):
+    if name[0].lower() == 'r':
+        return "{} plays banjo".format(name)
+    return "{} does not play banjo".format(name)
+
+# best solution
+def are_you_playing_banjo(name):
+    return name + (
+        ' plays ' if name[0].lower() == 'r'
+        else ' does not play '
+    ) + 'banjo'
