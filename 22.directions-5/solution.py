4eafc0e51cf7f28e4db98d7bf3c96e6cf4f92ae1
Egor <egorcoders@gmail.com>
2022-09-05 12:28:22 +0500

    ADDED solution for bro

--- 22.directions-5/solution.py
+++ 22.directions-5/solution.py
@@ -1,18 +1,41 @@
-# my solution
-def directions(arr: list) -> list:
-    '''
-    Задача 22. Выбор кратчайшего пути.
-    '''
+# # my solution
+# def directions(arr: list) -> list:
+#     '''
+#     Задача 22. Выбор кратчайшего пути.
+#     '''
+#     cases = {
+#         "NORTH": "SOUTH",
+#         "SOUTH": "NORTH",
+#         "EAST": "WEST",
+#         "WEST": "EAST"
+#     }
+#     stack = []
+#     for i in arr:
+#         if stack and stack[-1] == cases[i]:
+#             stack.pop()
+#         else:
+#             stack.append(i)
+#     return stack
+
+
+def fn(arr):
+    arr = ''.join(lst)
     cases = {
-        "NORTH": "SOUTH",
-        "SOUTH": "NORTH",
-        "EAST": "WEST",
-        "WEST": "EAST"
+        0: '()',
+        1: '{}',
+        2: '[]',
     }
-    stack = []
-    for i in arr:
-        if stack and stack[-1] == cases[i]:
-            stack.pop()
+    while True:
+        if cases[0] in arr:
+            arr = arr.replace(cases[0], '')
+        elif cases[1] in arr:
+            arr = arr.replace(cases[1], '')
+        elif cases[2] in arr:
+            arr = arr.replace(cases[2], '')
         else:
-            stack.append(i)
-    return stack
+            return arr
+
+
+lst = ['(', '(', ')', '}']
+
+print('Ответ хуевый:', fn(lst))
