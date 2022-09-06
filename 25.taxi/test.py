7e7f59999d20de4fac0d133922cc84f34e85b28e
Egor <egorcoders@gmail.com>
2022-09-06 23:31:51 +0500

    added 25.taxi

---
+++ 25.taxi/test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from case_result import case_result_lst as c_dct
+from solution import lift
+
+
+class TestFn(unittest.TestCase):
+    def test_case_result(self):
+        for c, r, t in c_dct:
+            self.assertEqual(
+                lift(c), r, f'There is an error in {t.upper()}'
+            )
+
+
+if __name__ == '__main__':
+    unittest.main()
