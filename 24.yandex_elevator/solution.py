b9a2777bfe24fee751a4ea5eb542b17ca50994fb
Egor <egorcoders@gmail.com>
2022-09-06 23:28:50 +0500

    updated

--- 24.yandex_elevator/solution.py
+++ 24.yandex_elevator/solution.py
@@ -5,12 +5,12 @@ k = 2  # шаг лифта
 up = 200  # стоимость подъема вверх
 down = 100  # стоимость подъема вниз
 
-k_lst = range(1, m, k)  # этажи остановки лифта
+k_floors = range(1, m, k)
 
 ans = min([
-    up * (n - i) if n > i  # если недоехали, считаем на поъем
-    else down * (i - n)  # если проехали, считаем на спуск
-    for i in k_lst
+    up * (n - i) if n > i
+    else down * (i - n)
+    for i in k_floors
 ])
 
 print(ans)
